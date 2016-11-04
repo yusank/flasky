a1efed1ca4a77ee7174b9e831f7c9da606a3d772
yusank <yusana@sina.cn>
2016-11-04 19:02:56 +0800

    upgrade for today

--- app/auth/views.py
+++ app/auth/views.py
@@ -10,33 +10,8 @@ from flask_login import login_user, logout_user, login_required, current_user
 from flask import render_template, redirect, request, url_for, flash
 from . import auth
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from ..email import send_email
-
-@auth.route('/login', methods = ['GET', 'POST'])
-def login():
-	form = LoginForm()
-	if form.validata_on_submit():
-		user = User.query.filter_by(email = form.email.data).first()
-		if user is not None and user.varify_password(form.password.data):
-			login_user(user,form.remember_me.data)
-			return redirect(request.args.get('next') or url_for('main.index'))
-		falsh('Invalid username or password.')
-	return render_template('auth/login.html', form = form)
-
-@auth.route('/register', methods = ['GET', 'POST'])
-def register():
-	form = RegistrationForm()
-	if form.validata_on_submit():
-		user = User(email = form.email.data,username = form.username.data, password = form.password.data)
-		db.session.add(user)
-		db.session.commit()
-		token = user.generate_confirmation_token()
-		send_email(user.email, 'Confirm your Account',
-							'auth/eamil/confirm',user = user, token = token)
-		flash('A confirmation eamil has been send to you by email.')
-		render_template(url_for('main.index'))
-	return render_template('auth/register.html', form = form)
 
 @auth.before_app_request
 def before_request():
@@ -45,17 +20,43 @@ def before_request():
 
 @auth.route('/unconfirmed')
 def unconfirmed():
-	if current_user.is_authenticated or current_user.confirmed:
+	if current_user.is_anonymous or current_user.confirmed:
 		return redirect(url_for('main.index'))
-	return redirect(url_for('auth/unconfirmed.html'))
+	return render_template('auth/unconfirmed.html')
 
-@auth.route('/confirm')
+@auth.route('/login', methods = ['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(email = form.email.data).first()
+		if user is not None and user.verify_password(form.password.data):
+			login_user(user,form.remember_me.data)
+			return redirect(request.args.get('next') or url_for('main.index'))
+		falsh('Invalid username or password.')
+	return render_template('auth/login.html', form = form)
+
+@auth.route('/logout')
 @login_required
-def resend_confirmation():
-	token = current_user.generate_confirmation_token()
-	send_email(current_user.eamil, 'Confirm your Account', 'auth/email/confirm',user = current_user,token = token)
-	flash('A new confirmation email has been send to your by email')
+def logout():
+	logout_user()
+	flash('You have been logged out.')
 	return redirect(url_for('main.index'))
+
+
+
+@auth.route('/register', methods = ['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(email = form.email.data,username = form.username.data, password = form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		token = user.generate_confirmation_token()
+		send_email(user.email, 'Confirm your Account',
+							'auth/email/confirm',user = user, token = token)
+		flash('A confirmation email has been send to you by email.')
+		return redirect(url_for('auth.login'))
+	return render_template('auth/register.html', form = form)
 
 @auth.route('/confirm/<token>')
 @login_required
@@ -68,9 +69,10 @@ def confirm(token):
 		flash('The confirmation link is invalid or has expired')
 	return redirect(url_for('main.index'))
 
-@auth.route('/logout')
+@auth.route('/confirm')
 @login_required
-def logout():
-	logout_user()
-	flash('You have been logged out.')
+def resend_confirmation():
+	token = current_user.generate_confirmation_token()
+	send_email(current_user.email, 'Confirm your Account', 'auth/email/confirm',user = current_user,token = token)
+	flash('A new confirmation email has been send to your by email')
 	return redirect(url_for('main.index'))
--- app/main/views.py
+++ app/main/views.py
@@ -6,22 +6,10 @@
  @DateTime:    2016-11-03 16:30:43
  @Description: app/main/views.py 
 '''
-from datetime import datetime
-from flask import render_template, session, redirect, url_for
-
+from flask import render_template
 from . import main
-from .forms import NameForm
-from .. import db
-from ..models import User
 
-@main.route('/', methods = ['GET','POST'])
+
+@main.route('/')
 def index():
-	form = NameForm()
-	if form.validata_on_subnit():
-		#...
-		return redirect(url_for('.index'))
-	return render_template('index.html',
-							form = form,
-							name = session.get('name'),
-							known = session.get('known',False),
-							current_time = datetime.utcnow())
+    return render_template('index.html')
