8fe256ac723c67d659efd741660f2d24d27bb3d1
yusank <yusana@sina.cn>
2016-11-16 22:45:22 +0800

    11.15

--- config.py
+++ config.py
@@ -21,6 +21,7 @@ class Config:
 	FLASKY_MAIL_SENDER = 'Flasky Admin <yusank@yeah.net>'
 	FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 	FLASKY_POSTS_PER_PAGE = 20
+	FLASKY_FOLLOWERS_PER_PAGE = 50
 
 	@staticmethod
 	def init_app(app):
